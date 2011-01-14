3eac1cd1b3b78fe694429dc942ee33e15d70e9bc
Christopher Dunn <cdunn2001@gmail.com>
2011-01-13 19:09:56 -0800

    After 'rails new .'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twoo::Application.config.secret_token = '8847bd10ee64840f3a01b69f1f790e6f44c4c7d08cccf5eb8e4402e79b1ae5413258044b28507ac810c858208b3b5e36d6f6880212f829f67185c4ec7c2f82e0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Twoo::Application.config.session_store :cookie_store, :key => '_twoo_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Twoo::Application.config.session_store :active_record_store
