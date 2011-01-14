193d5cdaa3c12888cd10dcd8b7ab24e0c668aee4
Christopher Dunn <cdunn2001@gmail.com>
2011-01-13 19:56:19 -0800

    App layout template, and default home page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Twoo::Application.routes.draw do
+  get "home/index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -46,11 +48,7 @@ Twoo::Application.routes.draw do
   #     resources :products
   #   end
 
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => "welcome#index"
-
-  # See how all your routes lay out with "rake routes"
+  root :to => "home#index"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
