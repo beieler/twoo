b950ff44d878ed5e450d3be476e2974fc2e8f09d
Christopher Dunn <cdunn2001@gmail.com>
2011-01-14 23:59:09 -0800

    Sign-in/out redirect seems to work now, even with public/index.html

--- config/routes.rb
+++ config/routes.rb
@@ -28,49 +28,17 @@ Twoo::Application.routes.draw do
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
   # This route can be invoked with purchase_url(:id => product.id)
 
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+  #namespace :user do
+  #  #root :to => "users#index"
+  #  root :to => "home#index" # does not work!
+  #end
+  #scope '/user' do
+  #  root :to => "home#index"
+  #end
 
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Sample resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Sample resource route with more complex sub-resources
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', :on => :collection
-  #     end
-  #   end
-
-  # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-
-#  namespace :user do
-#    root :to => "users#index"
-#  end
+  # For devise sign_in.
+  match '/home/index' => 'home#index', :as => 'user_root'
   
-  root :to => "home#index" # for devise, until we switch to above
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id(.:format)))'
+  # For devise sign_out.
+  root :to => "home#index" # no effect until we del public/index.html
 end
